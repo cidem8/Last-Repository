e763552be71a8c4379c6f12b9ee5d1caadc6f66b
cidem8 <cigdem_ay88@hotmail.com>
2021-12-10 11:25:39 +0300

    Add New Method-Pascal Casing

--- SolutionApp01/WorkSpace/Program.cs
+++ SolutionApp01/WorkSpace/Program.cs
@@ -7,7 +7,32 @@ namespace WorkSpace
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string message = "WorkSpace!";
+            Console.WriteLine(message);
+            People people = new People();
+            
+          
+            
             Console.ReadLine();
         }
+
+    
+    }
+    public class People
+    {
+
+        //public string name = "Çiğdem";
+        //public string surname = "ay";
+        //public int birthYear = 1988;
+        //public long TCNumber = 1234567890;
+
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int BirthDate { get; set; }
+        public long TcNumber { get; set; }
+
+
+
     }
 }
